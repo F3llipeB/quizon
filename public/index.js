0d764a9a5c6b47e03b8508a4033a5545732ac063
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-04-07 22:52:53 -0300

    Adicionando contatos

--- public/index.js
+++ public/index.js
@@ -1,13 +1,19 @@
 const aboutBtn = document.getElementById("about-link");
 const sectionAbout = document.getElementById("sectionAbout");
-const sectionExperiences = document.getElementById("experiences-section")
+const sectionExperiences = document.getElementById("experiences-section");
+const contact = document.getElementById("contact")
+const contactBtn = document.getElementById("contactbtn")
 //Função que ao clicar o botão about me, será levado para determinada área
 
 aboutBtn.addEventListener("click", ()=>{
     sectionAbout.scrollIntoView({behavior:"smooth"});
     
 })
+//Função que ao clicar o botão contact me, será levado para determinada área
 
+contactBtn.addEventListener("click", function(){
+    contact.scrollIntoView({behavior:"smooth"})
+})
 // Função para adicionar novas experiencias na seção de experiencias
 
 const experiences = [
